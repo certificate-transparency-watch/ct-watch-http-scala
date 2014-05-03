a5d538e26b625c47391551d03578c10e10fd2ab4
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-05-03 09:50:12 +0100

    Replace traits with classes
    This seems unscalaesque. I expect I'll convert back to traits
    eventually.
    
    By doing this, I hope to learn more about what traits/cake pattern
    provides.

--- src/main/scala/com/example/Boot.scala
+++ src/main/scala/com/example/Boot.scala
@@ -7,8 +7,8 @@ import spray.can.Http
 object Boot extends App {
 
   val system = ActorSystem("on-spray-can")
-
+  
   val service = system.actorOf(Props[MyServiceActor], "demo-service")
 
-  IO(Http)(system) ! Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http)(system) ! Http.Bind(service, interface = "localhost", port = 8088)
 }
--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -5,25 +5,25 @@ import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 
-class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
-  def actorRefFactory = system
+class MyServiceSpec extends Specification with Specs2RouteTest {
+  val myService = new MyService(system)
   
   "MyService" should {
 
     "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
+      Get() ~> myService.myRoute ~> check {
         responseAs[String] must contain("Say hello")
       }
     }
 
     "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
+      Get("/kermit") ~> myService.myRoute ~> check {
         handled must beFalse
       }
     }
 
     "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
+      Put() ~> myService.sealRoute(myService.myRoute) ~> check {
         status === MethodNotAllowed
         responseAs[String] === "HTTP method not allowed, supported methods: GET"
       }
