29584560f29cdaab5645f77f3455c8fe590adb5e
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-05-03 12:14:06 +0100

    Remove useless comments

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -5,13 +5,7 @@ import spray.routing._
 import spray.http._
 import MediaTypes._
 
-// we don't implement our route structure directly in the service actor because
-// we want to be able to test it independently, without having to spin up an actor
 class MyServiceActor(myService : MyService) extends Actor {
-
-  // this actor only runs our route, but you could add
-  // other things here, like request stream processing
-  // or timeout handling
   def receive = myService.runRoute(myService.myRoute)
 }
 
@@ -22,7 +16,6 @@ class LogServerRepository {
 case class LogServer(id: Int, name: String)
 
 
-// this trait defines our service behavior independently from the service actor
 class MyService(val actorRefFactory : ActorRefFactory, logServerRepository: LogServerRepository) extends HttpService {
 
   val myRoute =
