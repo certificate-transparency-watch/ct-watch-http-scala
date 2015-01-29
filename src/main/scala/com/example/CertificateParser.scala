cf53b08b1ae39aa18b67d3f3f93a77d3508d5849
Tom <thomasf@blinkbox.com>
2015-01-29 17:42:50 +0000

    Add issuer DN to cert parser

--- src/main/scala/com/example/CertificateParser.scala
+++ src/main/scala/com/example/CertificateParser.scala
@@ -8,7 +8,7 @@ import org.bouncycastle.asn1.x500.style.{IETFUtils, BCStyle}
 import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder
 import org.joda.time.DateTime
 
-case class Certificate(serialNumber: BigInt, commonName: String, subjectAlternativeNames: Seq[String], expiry: DateTime)
+case class Certificate(serialNumber: BigInt, commonName: String, subjectAlternativeNames: Seq[String], expiry: DateTime, issuerDN: String)
 
 class CertificateParser {
 
@@ -17,9 +17,10 @@ class CertificateParser {
     val x509cert: X509Certificate = cf.generateCertificate(new ByteArrayInputStream(c)).asInstanceOf[X509Certificate]
     val cert = new JcaX509CertificateHolder(x509cert)
     val cn = IETFUtils.valueToString(cert.getSubject.getRDNs(BCStyle.CN)(0).getFirst.getValue)
+    val issuerDN = IETFUtils.valueToString(cert.getIssuer.getRDNs(BCStyle.CN)(0).getFirst.getValue)
     val sans = x509cert.getSubjectAlternativeNames.map { l: java.util.List[_] =>
       l.get(1).asInstanceOf[String]
     }.toSeq
-    Certificate(x509cert.getSerialNumber, cn, sans, new DateTime(cert.getNotAfter))
+    Certificate(x509cert.getSerialNumber, cn, sans, new DateTime(cert.getNotAfter), issuerDN)
   }
 }
